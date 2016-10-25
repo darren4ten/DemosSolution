43b411c8d6b718a56ba00ad96861bea16e588a7a
tengda <tengda@yiguo.com>
2016-10-25 10:46:15 +0800

    C#中跨线程访问控件问题解决方案

---
+++ DemosSolution/Winform多线程控件控制/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Winform多线程控件控制
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(54, 28);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 12);
+            this.lblTime.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 188);
+            this.Controls.Add(this.lblTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ DemosSolution/Winform多线程控件控制/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform多线程控件控制
+{
+    public partial class Form1 : Form
+    {
+        Thread t;
+        private bool isStopped = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetNoCheckFOrIllegalCrossThreadCalls();
+            // StartTimer();
+            //StartTimer1();
+            //StartTimer2();
+        }
+
+        #region 会出错的多线程调用
+        /// <summary>
+        /// 会报错的多线程调用
+        /// System.InvalidOperationException”类型的未经处理的异常在 System.Windows.Forms.dll 中发生 
+        ///其他信息: 线程间操作无效: 从不是创建控件“lblTime”的线程访问它。
+        /// </summary>
+        void StartTimer()
+        {
+
+            Thread t = new Thread(() =>
+            {
+                lblTime.Text = DateTime.Now.ToString("yyyy/MM/DD HH:mm:ss");
+                Thread.Sleep(1000);
+            });
+            t.IsBackground = true;
+            t.Start();
+        }
+        #endregion
+
+        #region 方法0： 全局设置允许跨线程调用控件， Control.CheckForIllegalCrossThreadCalls = false;
+        void SetNoCheckFOrIllegalCrossThreadCalls()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            StartTimer1();
+        }
+        #endregion
+
+        #region 方法1：使用代理来解决跨线程调用控件问题,需要父级控件中调用Invoke
+        public delegate void DoWorkDelegate(string msg);
+        void StartTimer1()
+        {
+            // Thread.
+            t = new Thread(() =>
+            {
+                string p = "StartTimer1";
+                DoWorkDelegate d = new DoWorkDelegate((pt) =>
+                {
+
+                    lblTime.Text = pt + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                });
+                while (!isStopped)
+                {
+                    this.Invoke(d, p);
+                    Thread.Sleep(2000);
+                }
+            });
+            t.IsBackground = true;
+            t.Start();
+        }
+        #endregion
+
+        #region 方法2：使用MethodInvoke方法来实现跨线程调用,其原理和方法1一样，只是用MethodInvoker代替自定义的Delegate
+        void StartTimer2()
+        {
+            t = new Thread(() =>
+            {
+                MethodInvoker invoker = new MethodInvoker(() =>
+                {
+
+                    lblTime.Text = "StartTimer2:" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                });
+                while (!isStopped)
+                {
+                    this.Invoke(invoker);
+                    Thread.Sleep(2000);
+                }
+            });
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        #endregion
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isStopped = true;
+            if (t != null)
+            {
+                t.Abort();
+            }
+        }
+    }
+}
